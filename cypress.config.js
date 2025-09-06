9e06e3eb0c804d4d24a620725bb2f4c479fab0bb
QADavidUraga <daviduraga@outlook.es>
2025-09-06 14:38:36 -0600

    Ajustes

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,21 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://programamazsalud.com.mx',
+    supportFile: 'cypress/support/e2e.js',
+    defaultCommandTimeout: 12000,
+    userAgent:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
     setupNodeEvents(on, config) {
-      // Plugins si los necesitas
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome') {
+          launchOptions.args.push('--disable-blink-features=AutomationControlled');
+          launchOptions.args.push('--lang=es-ES');
+        }
+        return launchOptions;
+      });
+      return config;
     },
-    baseUrl: "https://programamazsalud.com.mx/",
-    defaultCommandTimeout: 10000,
-    viewportWidth: 1280,
-    viewportHeight: 800,
-  }
+  },
 });
